ca4c3402f352cb5e9b08538975480d90bbb548b6
Hunter Sullivan <hcsullivan12@gmail.com>
2023-09-27 17:29:56 -0500

    Update setter/getter for storage for lines instead of chars.

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -9,55 +9,16 @@ class TestStorage(unittest.TestCase):
         lstg = LineStorage()
 
         for i in range(100):
-            self.assertEqual(lstg.char(randint(0, 100), randint(0, 100), randint(0, 100)), None)
+            self.assertEqual(lstg.get_line(randint(0, 100)), None)
             self.assertEqual(lstg.word(randint(0, 100)), 0)
 
     def test_single_line_stores_correctly(self):
         line = "Songs but chief has ham widow downs"
         lstg = LineStorage()
 
-        # store it
-        for w,word in enumerate(line.split(), 1):
-            for c,char in enumerate(word, 1):
-                lstg.set_char(1, w, c, char)
+        lstg.set_line(1, line)
 
-        # verify it
-        self.assertEqual(lstg.char(1, 1, 1), 'S')
-        self.assertEqual(lstg.char(1, 1, 2), 'o')
-        self.assertEqual(lstg.char(1, 1, 3), 'n')
-        self.assertEqual(lstg.char(1, 1, 4), 'g')
-        self.assertEqual(lstg.char(1, 1, 5), 's')
-
-        self.assertEqual(lstg.char(1, 2, 1), 'b')
-        self.assertEqual(lstg.char(1, 2, 2), 'u')
-        self.assertEqual(lstg.char(1, 2, 3), 't')
-
-        self.assertEqual(lstg.char(1, 3, 1), 'c')
-        self.assertEqual(lstg.char(1, 3, 2), 'h')
-        self.assertEqual(lstg.char(1, 3, 3), 'i')
-        self.assertEqual(lstg.char(1, 3, 4), 'e')
-        self.assertEqual(lstg.char(1, 3, 5), 'f')
-
-        self.assertEqual(lstg.char(1, 4, 1), 'h')
-        self.assertEqual(lstg.char(1, 4, 2), 'a')
-        self.assertEqual(lstg.char(1, 4, 3), 's')
-
-        self.assertEqual(lstg.char(1, 5, 1), 'h')
-        self.assertEqual(lstg.char(1, 5, 2), 'a')
-        self.assertEqual(lstg.char(1, 5, 3), 'm')
-
-        self.assertEqual(lstg.char(1, 6, 1), 'w')
-        self.assertEqual(lstg.char(1, 6, 2), 'i')
-        self.assertEqual(lstg.char(1, 6, 3), 'd')
-        self.assertEqual(lstg.char(1, 6, 4), 'o')
-        self.assertEqual(lstg.char(1, 6, 5), 'w')
-
-        self.assertEqual(lstg.char(1, 7, 1), 'd')
-        self.assertEqual(lstg.char(1, 7, 2), 'o')
-        self.assertEqual(lstg.char(1, 7, 3), 'w')
-        self.assertEqual(lstg.char(1, 7, 4), 'n')
-        self.assertEqual(lstg.char(1, 7, 5), 's')
-
+        self.assertEqual(lstg.get_line(1), line)
         self.assertEqual(lstg.word(1), 7)
 
     def test_single_line_error_returns(self):
@@ -65,31 +26,20 @@ class TestStorage(unittest.TestCase):
         lstg = LineStorage()
 
         # test <= 0
-        self.assertEquals(lstg.word(1), 0)
-        lstg.set_char(0, 1, 1, 0)
-        lstg.set_char(1, 0, 1, 1)
-        lstg.set_char(1, 1, 0, 1)
-        lstg.set_char(-1, 1, 1, 1)
-        lstg.set_char(1, -1, 1, 1)
-        lstg.set_char(1, 1, -1, 1)
-        self.assertEquals(lstg.word(1), 0)
+        self.assertEqual(lstg.word(1), 0)
+        lstg.set_line(0, 1)
+        lstg.set_line(-1, 1)
+        self.assertEqual(lstg.word(1), 0)
 
         # store it
-        for w,word in enumerate(line.split(), 1):
-            for c,char in enumerate(word, 1):
-                lstg.set_char(1, w, c, char)
+        lstg.set_line(1, line)
 
-        # test char
+        # test line
         for i in range(100):
-            r1, r2, r3 = randint(-100, 100), randint(-100, 100), randint(-100, 100)
+            r1 = randint(-100, 100)
             while r1 == 1:
                 r1 = randint(-100, 100)
-            while r2 == 1:
-                r2 = randint(-100, 100)
-            while r3 == 1:
-                r3 = randint(-100, 100)
-
-            self.assertEqual(lstg.char(r1, r2, r3), None)
+            self.assertEqual(lstg.get_line(r1), None)
 
         # test word
         for i in range(100):
@@ -109,41 +59,10 @@ class TestStorage(unittest.TestCase):
 
         # store it
         for l,line in enumerate(lines, 1):
-            for w,word in enumerate(line.split(), 1):
-                for c,char in enumerate(word, 1):
-                    lstg.set_char(l, w, c, char)
+            lstg.set_line(l, line)
 
         # verify it
-        self.assertEqual(lstg.char(1, 1, 1), 'G')
-        self.assertEqual(lstg.char(1, 1, 2), 'e')
-        self.assertEqual(lstg.char(1, 1, 3), 'n')
-        self.assertEqual(lstg.char(1, 1, 4), 'i')
-        self.assertEqual(lstg.char(1, 1, 5), 'u')
-        self.assertEqual(lstg.char(1, 1, 6), 's')
-
-        self.assertEqual(lstg.char(2, 1, 1), 'L')
-        self.assertEqual(lstg.char(2, 1, 2), 'a')
-        self.assertEqual(lstg.char(2, 1, 3), 'r')
-        self.assertEqual(lstg.char(2, 1, 4), 'g')
-        self.assertEqual(lstg.char(2, 1, 5), 'e')
-
-        self.assertEqual(lstg.char(2, 2, 1), 'd')
-        self.assertEqual(lstg.char(2, 2, 2), 'o')
-
-        self.assertEqual(lstg.char(2, 3, 1), 't')
-        self.assertEqual(lstg.char(2, 3, 2), 'r')
-        self.assertEqual(lstg.char(2, 3, 3), 'i')
-        self.assertEqual(lstg.char(2, 3, 4), 'e')
-        self.assertEqual(lstg.char(2, 3, 5), 'd')
-
-        self.assertEqual(lstg.char(3, 1, 1), 'S')
-        self.assertEqual(lstg.char(3, 1, 2), 'i')
-        self.assertEqual(lstg.char(3, 1, 3), 'l')
-        self.assertEqual(lstg.char(3, 1, 4), 'e')
-        self.assertEqual(lstg.char(3, 1, 5), 'n')
-        self.assertEqual(lstg.char(3, 1, 6), 't')
-
-        self.assertEqual(lstg.word(1), 6)
-        self.assertEqual(lstg.word(2), 7)
-        self.assertEqual(lstg.word(3), 9)
-        self.assertEqual(lstg.word(4), 8)
+        self.assertEqual(lstg.get_line(1), lines[0])
+        self.assertEqual(lstg.get_line(2), lines[1])
+        self.assertEqual(lstg.get_line(3), lines[2])
+        self.assertEqual(lstg.get_line(4), lines[3])
