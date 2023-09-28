c61b5e30ebc85f5da23d6bc224730fdb39c648d5
Hunter Sullivan <hcsullivan12@gmail.com>
2023-09-27 20:50:21 -0500

    Reject siple noise words.

--- kwic/output.py
+++ kwic/output.py
@@ -13,16 +13,28 @@ COLORS.append('\033[35m')
 COLORS.append('\033[36m')
 COLORS.append('\033[37m')
 
+NOISE_WORDS = ['with', 'then', 'they', 'the', 'and', 'or', 'you', 'from']
+
 class Output(object):
     def __init__(self, shifter):
         self._shifter = shifter
 
     def _print_line(self, line):
-        x = random.randint(0,6)
+        x = random.randint(0,len(COLORS)-1)
         print(COLORS[x] + line + ENDC)
+
+    def _has_noise_word(self, line):
+        word = line.split()[0].lower()
+        if len(word) <= 2:
+            return True
+        elif word in NOISE_WORDS:
+            return True
+        return False
 
     def get_output(self):
         print("Getting output...")
         print("\n")
         for line in self._shifter.get_sorted_lines():
+            if self._has_noise_word(line):
+                continue
             self._print_line(line)
