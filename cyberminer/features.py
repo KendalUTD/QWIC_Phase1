4e59e28c673792dc8f151b1bb32e7043fa789707
CH Divyesh Venkata Rama Kumar <88731571+ChDivyesh@users.noreply.github.com>
2023-11-19 20:14:50 -0600

    Update features, seepages
    Added Set_pages method

--- cyberminer/features.py
+++ cyberminer/features.py
@@ -22,13 +22,24 @@ class SearchEngine:
         allowed_symbols = string.ascii_letters + string.digits
         filtered_query = ''.join(char for char in query if char in allowed_symbols)
         return filtered_query
+        
+    def set_pages(results, results_per_page, page_number):
+        start_index = (page_number - 1) * results_per_page
+        end_index = start_index + results_per_page
+        current_page_results = results[start_index:end_index]
+        return current_page_results
 
     def handle_search_request(self, event):
         # Simulated search logic
         query = event.query
         filtered_query = self.filter_symbols(query)
         results = self.perform_search(filtered_query)
-        event_bus.publish(SearchResultEvent(results))
+        # Example: Set the number of results to show per page and navigate to page 1
+        results_per_page = 3
+        current_page_number = 1
+        current_page_results = self.set_pages(results, results_per_page, current_page_number)
+
+        event_bus.publish(SearchResultEvent(current_page_results))
 
     def perform_search(self, query):
         # implementation omitted
